7f0f91f9dff1acfe27bdd4b16793ec6498ea0b06
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-05-16 19:47:56 +0530

    feat: fetch tasks from database sorted by creation date

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -3,5 +3,7 @@ import { PrismaClient } from "../generated/prisma/client";
 const prisma = new PrismaClient();
 
 export default async function TasksPage() {
-  
+  const tasks = await prisma.task.findMany({
+    orderBy: { createdAt: "desc" },
+  });
 }
